0097b6d444722bcd3a08a086dda29b0de056b76f
Sol <sol@Sols-MBP.ccs>
2017-04-12 14:01:30 -0400

    Change to React Webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
+var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
-  entry: './app/index.js',
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
   }
 };
+
+module.exports = config;
